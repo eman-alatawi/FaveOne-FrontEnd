7ecce70d3d01c92b178963f5dd00e0081991f8b3
eman-e-alatawi <eman.eid.alatawi@gmail.com>
2021-03-02 20:22:47 +0300

    change the font size in ActorDetails

--- src/actor/ActorDetails.js
+++ src/actor/ActorDetails.js
@@ -6,20 +6,20 @@ export default function ActorDetails(props) {
         <div>
             <div className=" flex flex-row  bg-gray-50  rounded-lg shadow p-10 ">
                 <div className="h-96 w-72  mr-10 flex flex-col bg-gray-800 text-gray-300">
-                    <img className=" h-72 w-72 mr-10 shadow  border-4 border-pink-100 object-cover" src={props.actor.picture} />
+                    <img className=" h-72 w-72 mr-10 shadow   object-cover" src={props.actor.picture} />
                     <div className="w-64 mt-3  text-center flex flex-col">
-                        <h4>Social Account:</h4>
+                        <h1>Social Account:</h1>
                         {props.actor.socialAccount}
                     </div>
                 </div>
                 <div className=" w-full flex flex-col pt-2">
                     <div className=" flex flex-row justify-start w-full mb-4">
-                        <h4 className="mr-20">{props.actor.fullName}</h4>
-                        <h4 className="mr-20">{props.actor.dateOfBirth}</h4>
-                        <h4>{props.actor.gender}</h4>
+                        <h1 className="mr-20 text-xl">{props.actor.fullName}</h1>
+                        <h1 className="mr-20 text-xl">{props.actor.dateOfBirth}</h1>
+                        <h1>{props.actor.gender}</h1>
                     </div>
 
-                    <h4>Biography:</h4>
+                    <h1 className="text-xl">Biography:</h1>
                     <p className="text-justify w-5/5 pr-5 h-72 overflow-y-scroll">{props.actor.biography}</p>
                 </div>
             </div>
