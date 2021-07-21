b806404019ac45fce97caa9c4be2cdc4b190d603
eman-e-alatawi <eman.eid.alatawi@gmail.com>
2021-07-21 23:17:33 +0300

    Better Responsive design for ActorDetails

--- src/actor/ActorDetails.js
+++ src/actor/ActorDetails.js
@@ -20,37 +20,37 @@ export default function ActorDetails(props) {
   console.log(actor.fullName);
   return (
     <div>
-      <div className=" flex flex-row  bg-gray-50  rounded-lg shadow p-10 ">
-        <div className="h-96 w-72  mr-10 flex flex-col bg-gray-800 text-gray-300">
+      <div className=" flex flex-col md:flex-row   bg-gray-50  rounded-lg shadow p-10 ">
+        <div className="h-96 w-full md:w-80   mr-10 flex flex-col bg-gray-800 text-gray-300">
           <img
-            className=" h-72 w-72 mr-10 shadow   object-cover"
+            className=" h-80 w-full md:w-80  mr-10 shadow   object-cover"
             src={actor.picture}
           />
-          <div className="w-64 mt-3  text-center flex flex-col">
+          <div className="w-full  flex flex-col text-center py-1">
             <h1 className="text-xl">Social Account:</h1>
-            {actor.socialAccount}
+            <p>{actor.socialAccount}</p>
           </div>
         </div>
         <div className=" w-full flex flex-col pt-2">
-          <div className=" p-4 flex flex-row justify-start w-full mb-4 rounded-lg bg-gray-800 text-gray-300">
+          <div className=" p-4 flex flex-col md:flex-row justify-start w-full mb-4 rounded-lg bg-gray-800 text-gray-300">
             <h1 className="mr-20 text-xl">{actor.fullName}</h1>
             <h1 className="mr-20 text-xl">{actor.dateOfBirth}</h1>
             <h1 className="text-xl">{actor.gender}</h1>
           </div>
 
-          <h1 className="text-xl mb-3">Biography:</h1>
-          <p className="text-justify w-5/5 pr-5 h-72 leading-8 overflow-auto">
+          <h1 className="text-2xl mb-3 px-4 ">Biography:</h1>
+          <p className="text-justify w-5/5 px-4 h-72 leading-8 overflow-auto">
             {actor.biography} 
           </p>
         </div>
       </div>
 
       {actor.movieDramas && (
-        <div className=" flex flex-col  bg-gray-800  rounded-lg shadow p-10  w-full overflow-x-scroll ">
+        <div className=" flex flex-col items-center md:items-start  bg-gray-800  rounded-lg shadow p-10  w-full overflow-x-scroll ">
           <h4 className="text-gray-300 mb-3 text-xl ml-3">
             Related Movies/Dramas:
           </h4>
-          <div className="flex flex-row ">
+          <div className="flex flex-col gap-y-3 md:flex-row ">
             {" "}
             {actor.movieDramas.map((md, index) => (
               <div key={index}>
