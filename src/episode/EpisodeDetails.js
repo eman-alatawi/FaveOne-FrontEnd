1bf9a32816f6766e66cc334ad8aea41d831690dc
eman-e-alatawi <eman.eid.alatawi@gmail.com>
2021-07-21 23:43:59 +0300

    Better Responsive design for EpisodeDetails

--- src/episode/EpisodeDetails.js
+++ src/episode/EpisodeDetails.js
@@ -62,17 +62,17 @@ export default function EpisodeDetails(props) {
   return (
     <div className=" bg-gray-50 rounded-lg shadow">
       <div className="h-full w-full  ">
-        <h3 className="  mt-3 text-center text-gray-900 text-3xl opacity-75">
+        <h3 className="  mt-2 text-center text-gray-900 text-xl md:text-3xl opacity-75">
           Enjoy Watching
         </h3>
       </div>
-      <div className=" flex flex-row p-10 ">
-        <div className="h-96 w-80  mr-10 flex flex-col rounded-lg bg-pink-900 text-gray-300">
+      <div className=" flex flex-col md:flex-row items-center  p-10 ">
+        <div className="h-96 w-full md:w-80  flex flex-col rounded-lg bg-pink-900 text-gray-300">
           <img
-            className=" h-80 w-80 mr-10 shadow  object-cover "
+            className=" h-80 w-full md:w-80  shadow  object-cover "
             src={episode.thumbnail}
           />
-          <div className="w-64  text-center flex flex-col">
+          <div className="  text-center flex flex-col">
             {props.moviesDramas.map(
               (md, index) =>
                 md.episodes.findIndex((x) => x.id == episode.id) !== -1 && (
@@ -94,17 +94,17 @@ export default function EpisodeDetails(props) {
             <div className="">Episode {episode.episodNum}</div>
           </div>
         </div>
-        <div className=" w-4/5 flex flex-col pt-2 ">
+        <div className=" w-full md:w-3/5 h-96 flex flex-col pt-2 mx-3">
           {/* light={props.episode.thumbnail} */}
           <ReactPlayer
             url={episode.episodeVideoUrl}
             controls={true}
-            width="800px"
-            height="375px"
+            width='100%'
+            height='100%'
           />
         </div>
 
-        <div className="w-2/12  text-center pt-10 mt-2 mb-4 flex flex-col bg-gray-700 shadow-xl text-gray-300">
+        <div className="md:w-2/12 w-full text-center pt-10 mt-2 mb-4 flex flex-col bg-gray-700 shadow-xl text-gray-300">
           <h5 className="text-center">All Episodes: </h5>
 
           <ul className="text-left  h-72 overflow-auto">{relatedEpisodes}</ul>
